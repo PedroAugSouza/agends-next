dae2054ba250f3c6f1cac572d774485c93af84dc
PedroAugSouza <psouza.880@icloud.com>
2025-04-25 20:09:21 -0300

    feat: resizable principal sidebar

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -57,9 +57,13 @@ import {
 } from '@/shared/components/ui/sheet';
 import { useCalendar } from '@/shared/hooks/useCalendar';
 import { DEFAULT_SETTING_API } from '@/shared/constants/default-setting-api';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/shared/components/ui/resizable';
 
 export const HomeModule = () => {
-
   const { getEventsByDay, tags, refreshTags, currentDate, setCurrentDate } =
     useCalendar();
 
@@ -138,80 +142,90 @@ export const HomeModule = () => {
           </Popover>
         </div>
       </nav>
-      <div className="flex h-full w-full flex-row items-center justify-center">
-        <div className="flex h-full w-80 items-start justify-start border-r border-gray-200 bg-gray-100 px-6 py-2.5">
-          <Accordion
-            type="multiple"
-            className="w-full"
-            defaultValue={['item-1', 'item-2']}
-          >
-            <AccordionItem value="item-1">
-              <AccordionHeader className="flex flex-row items-center justify-between">
-                <span className="text-base font-semibold text-gray-700">
-                  Minha agenda
-                </span>
-                <div className="flex items-center gap-1 text-gray-500">
-                  <button
-                    onClick={() => setToggleAddTag(!toggleAddTag)}
-                    className="cursor-pointer"
-                  >
-                    {toggleAddTag ? <Minus size={22} /> : <Plus size={22} />}
-                  </button>
 
-                  <AccordionTrigger className="group w-full cursor-pointer items-center justify-between">
-                    <ChevronDown
-                      size={22}
-                      className="transition-all group-data-[state=open]:rotate-180"
-                    />
-                  </AccordionTrigger>
-                </div>
-              </AccordionHeader>
-              <AccordionContent className="flex flex-col items-center gap-1">
-                {tags?.map((tag) => (
-                  <Popover key={tag.uuid}>
-                    <PopoverTrigger className="group flex w-full cursor-pointer items-center justify-between rounded border border-transparent px-2 py-1 hover:bg-white data-[state=open]:border-gray-200 data-[state=open]:bg-white">
-                      <div className="flex items-center gap-2.5">
-                        <div
-                          className="size-3 rounded-full"
-                          style={{ backgroundColor: tag.color }}
-                        />
-                        <span className="text-sm font-normal text-gray-700">
-                          {tag.name}
-                        </span>
-                      </div>
-                      <CircleEllipsis
-                        size={20}
-                        strokeWidth={1.5}
-                        color={colors.gray[600]}
-                        className="hidden group-hover:flex group-data-[state=open]:flex"
-                      />
-                    </PopoverTrigger>
-                    <PopoverContent
-                      side="right"
-                      className="flex w-fit flex-col items-start justify-start gap-2.5 p-1"
+      <div className="flex h-full w-full flex-row items-center justify-center">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="flex h-full w-full"
+        >
+          <ResizablePanel
+            className="flex h-full items-start justify-start border-r border-gray-200 bg-gray-100 px-6 py-2.5"
+            defaultSize={12}
+            maxSize={20}
+            minSize={9}
+          >
+            <Accordion
+              type="multiple"
+              className="w-full"
+              defaultValue={['item-1', 'item-2']}
+            >
+              <AccordionItem value="item-1">
+                <AccordionHeader className="flex flex-row items-center justify-between">
+                  <span className="text-base font-semibold text-gray-700">
+                    Minha agenda
+                  </span>
+                  <div className="flex items-center gap-1 text-gray-500">
+                    <button
+                      onClick={() => setToggleAddTag(!toggleAddTag)}
+                      className="cursor-pointer"
                     >
-                      <PopoverArrow className="fill-white" />
-                      <Button
-                        size="sm"
-                        className="cursor-pointer rounded bg-gray-50 text-zinc-700 hover:bg-gray-100"
-                        onClick={() =>
-                          removeTagsControllerHandle(
-                            tag.uuid,
-                            DEFAULT_SETTING_API,
-                          ).then(() => {
-                            refreshTags();
-                          })
-                        }
+                      {toggleAddTag ? <Minus size={22} /> : <Plus size={22} />}
+                    </button>
+
+                    <AccordionTrigger className="group w-full cursor-pointer items-center justify-between">
+                      <ChevronDown
+                        size={22}
+                        className="transition-all group-data-[state=open]:rotate-180"
+                      />
+                    </AccordionTrigger>
+                  </div>
+                </AccordionHeader>
+                <AccordionContent className="flex flex-col items-center gap-1">
+                  {tags?.map((tag) => (
+                    <Popover key={tag.uuid}>
+                      <PopoverTrigger className="group flex w-full cursor-pointer items-center justify-between rounded border border-transparent px-2 py-1 hover:bg-white data-[state=open]:border-gray-200 data-[state=open]:bg-white">
+                        <div className="flex items-center gap-2.5">
+                          <div
+                            className="size-3 rounded-full"
+                            style={{ backgroundColor: tag.color }}
+                          />
+                          <span className="text-sm font-normal text-gray-700">
+                            {tag.name}
+                          </span>
+                        </div>
+                        <CircleEllipsis
+                          size={20}
+                          strokeWidth={1.5}
+                          color={colors.gray[600]}
+                          className="hidden group-hover:flex group-data-[state=open]:flex"
+                        />
+                      </PopoverTrigger>
+                      <PopoverContent
+                        side="right"
+                        className="flex w-fit flex-col items-start justify-start gap-2.5 p-1"
                       >
-                        Excluir
-                      </Button>
-                    </PopoverContent>
-                  </Popover>
-                ))}
-              </AccordionContent>
-            </AccordionItem>
-            {toggleAddTag && <TagForm setToggleAddTag={setToggleAddTag} />}
-            {/* <AccordionItem value="item-2">
+                        <PopoverArrow className="fill-white" />
+                        <Button
+                          size="sm"
+                          className="cursor-pointer rounded bg-gray-50 text-zinc-700 hover:bg-gray-100"
+                          onClick={() =>
+                            removeTagsControllerHandle(
+                              tag.uuid,
+                              DEFAULT_SETTING_API,
+                            ).then(() => {
+                              refreshTags();
+                            })
+                          }
+                        >
+                          Excluir
+                        </Button>
+                      </PopoverContent>
+                    </Popover>
+                  ))}
+                </AccordionContent>
+              </AccordionItem>
+              {toggleAddTag && <TagForm setToggleAddTag={setToggleAddTag} />}
+              {/* <AccordionItem value="item-2">
               <AccordionHeader className="flex flex-row items-center justify-between">
                 <span className="text-base font-semibold text-gray-700">
                   Meus hábitos
@@ -233,107 +247,110 @@ export const HomeModule = () => {
                 Yes. It adheres to the WAI-ARIA design pattern.
               </AccordionContent>
             </AccordionItem> */}
-          </Accordion>
-        </div>
-        <div className="flex h-full flex-1 flex-col items-center pr-6 pb-2.5 pl-2.5">
-          <nav className="ites-center flex h-12 w-full justify-between border-b border-gray-200 p-2">
-            <div className="flex items-center">
-              <span className="w-40 text-xl font-medium text-gray-600">
-                <strong className="text-gray-800">
-                  {getMonth(currentDate.getMonth())}
-                </strong>{' '}
-                {currentDate.getFullYear()}
-              </span>
+            </Accordion>
+          </ResizablePanel>
+          <ResizableHandle className="bg-transparent" />
 
-              <div className="flex items-center gap-1.5">
-                <button
-                  className="cursor-pointer text-violet-700"
-                  onClick={handlePrevMonth}
-                >
-                  <ChevronLeft size={22} strokeWidth={1.5} />
-                </button>
-                <button
-                  className="cursor-pointer rounded bg-gray-100 px-2 py-1 text-violet-700"
-                  onClick={() => setCurrentDate(new Date())}
-                >
-                  Hoje
-                </button>
-                <button
-                  className="cursor-pointer text-violet-700"
-                  onClick={handleNextMonth}
-                >
-                  <ChevronRight size={22} strokeWidth={1.5} />
-                </button>
-              </div>
-            </div>
+          <ResizablePanel className="flex h-full flex-1 flex-col items-center pr-6 pb-2.5 pl-2.5">
+            <nav className="ites-center flex h-12 w-full justify-between border-b border-gray-200 p-2">
+              <div className="flex items-center">
+                <span className="w-40 text-xl font-medium text-gray-600">
+                  <strong className="text-gray-800">
+                    {getMonth(currentDate.getMonth())}
+                  </strong>{' '}
+                  {currentDate.getFullYear()}
+                </span>
 
-            <div className="flex items-center gap-4">
-              <Select>
-                <SelectTrigger
-                  className="h-8 w-32 border-gray-300 p-2.5 shadow-none data-[placeholder]:text-gray-700"
-                  icon={<ChevronsUpDown />}
-                >
-                  <SelectValue
-                    placeholder="Mês"
-                    className="text-gray-600 placeholder:text-gray-600"
-                  />
-                </SelectTrigger>
-              </Select>
-
-              <Sheet>
-                <SheetTrigger asChild>
-                  <button className="cursor-pointer">
-                    <CirclePlus
-                      color={colors.gray[500]}
-                      strokeWidth={1.5}
-                      size={26}
-                    />
+                <div className="flex items-center gap-1.5">
+                  <button
+                    className="cursor-pointer text-violet-700"
+                    onClick={handlePrevMonth}
+                  >
+                    <ChevronLeft size={22} strokeWidth={1.5} />
                   </button>
-                </SheetTrigger>
+                  <button
+                    className="cursor-pointer rounded bg-gray-100 px-2 py-1 text-violet-700"
+                    onClick={() => setCurrentDate(new Date())}
+                  >
+                    Hoje
+                  </button>
+                  <button
+                    className="cursor-pointer text-violet-700"
+                    onClick={handleNextMonth}
+                  >
+                    <ChevronRight size={22} strokeWidth={1.5} />
+                  </button>
+                </div>
+              </div>
 
-                <SheetContent className="w-xs border border-gray-300 p-4">
-                  <SheetTitle className="font-normal text-gray-700">
-                    Novo evento
-                  </SheetTitle>
-                  <AddEventForm />
-                </SheetContent>
-              </Sheet>
+              <div className="flex items-center gap-4">
+                <Select>
+                  <SelectTrigger
+                    className="h-8 w-32 border-gray-300 p-2.5 shadow-none data-[placeholder]:text-gray-700"
+                    icon={<ChevronsUpDown />}
+                  >
+                    <SelectValue
+                      placeholder="Mês"
+                      className="text-gray-600 placeholder:text-gray-600"
+                    />
+                  </SelectTrigger>
+                </Select>
+
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <button className="cursor-pointer">
+                      <CirclePlus
+                        color={colors.gray[500]}
+                        strokeWidth={1.5}
+                        size={26}
+                      />
+                    </button>
+                  </SheetTrigger>
+
+                  <SheetContent className="w-xs border border-gray-300 p-4">
+                    <SheetTitle className="font-normal text-gray-700">
+                      Novo evento
+                    </SheetTitle>
+                    <AddEventForm />
+                  </SheetContent>
+                </Sheet>
+              </div>
+            </nav>
+            <div className="flex w-full grow items-center">
+              <Calendar
+                className="mt-2.5 h-full p-0 pt-2.5"
+                mode="single"
+                month={currentDate}
+                locale={ptBR}
+                fixedWeeks
+                classNames={{
+                  nav: 'hidden',
+                  caption: 'hidden',
+                  month: 'w-full grow h-full',
+                  root: 'w-full h-full',
+                  table: 'w-full grow  h-full flex flex-col h-full',
+                  tbody: 'flex flex-wrap grow justify-between ',
+                  head: 'w-full',
+                  head_row:
+                    ' w-full grid grid-cols-7 border-b pb-2.5 border-gray-200',
+                  head_cell:
+                    'border-r last:border-none border-gray-200 text-gray-600 w-full items-start flex p-2.5 font-semibold capitalize',
+                  cell: 'flex w-full items-center justify-between grow h-full',
+                  day: cn(
+                    buttonVariants({ variant: 'ghost' }),
+                    'flex-1 h-full  p-0 font-normal aria-selected:opacity-100',
+                  ),
+                  months: ' h-full',
+                }}
+                components={{
+                  Day: (props) => (
+                    <Day {...props} events={getEventsByDay(props.date)} />
+                  ),
+                }}
+              />
             </div>
-          </nav>
-          <div className="flex w-full grow items-center">
-            <Calendar
-              className="mt-2.5 h-full p-0 pt-2.5"
-              mode="single"
-              month={currentDate}
-              locale={ptBR}
-              fixedWeeks
-              classNames={{
-                nav: 'hidden',
-                caption: 'hidden',
-                month: 'w-full grow h-full',
-                root: 'w-full h-full',
-                table: 'w-full grow  h-full flex flex-col h-full',
-                tbody: 'flex flex-wrap grow justify-between ',
-                head: 'w-full',
-                head_row:
-                  ' w-full grid grid-cols-7 border-b pb-2.5 border-gray-200',
-                head_cell:
-                  'border-r last:border-none border-gray-200 text-gray-600 w-full items-start flex p-2.5 font-semibold capitalize',
-                cell: 'flex w-full items-center justify-between grow h-full',
-                day: cn(
-                  buttonVariants({ variant: 'ghost' }),
-                  'flex-1 h-full  p-0 font-normal aria-selected:opacity-100',
-                ),
-                months: ' h-full',
-              }}
-              components={{
-                Day: (props) => (
-                  <Day {...props} events={getEventsByDay(props.date)} />
-                ),
-              }}
-            />
-          </div>
-        </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </main>
   );
